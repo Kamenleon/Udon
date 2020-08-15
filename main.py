0588775a28a2adf529cbefe1e4d7b27437e01dda
Kamenleon <49759672+Kamenleon@users.noreply.github.com>
2020-08-15 13:21:38 +0900

    debug消した

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def upload():
 
 
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     #app.run(host='0.0.0.0', port=8888)
     #port = int(os.getenv("PORT"))
-    app.run(debug=False)
+    app.run()
