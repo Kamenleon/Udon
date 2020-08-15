1c28f907db16de4eb6c82db17acff2676a476a10
y-yana <66319933+y-yana@users.noreply.github.com>
2020-08-15 23:14:34 +0900

    test

--- server/static/js/actList.js
+++ server/static/js/actList.js
@@ -1,10 +1,11 @@
-function getActData() {
+window.onload = function getActData() {
   //ここで結果を受け取り、任意の変数に代入する
-  $.get.JSON("actData.json", function (data) {
+  var ans = document.getElementById("ansName").value;
+  $.getJSON("actData.json", function (data) {
     //↓actDataの配列内を検索
     for (var i = 0; i < Object.keys(data.actData).length; i++) {
       //↓結果と一致するnameの各データを取得し、画面に出力する
-      if (/* 変数 == */ data.actData[i].name) {
+      if (ans == data.actData[i].name) {
         var parent = document.getElementById("result");
         var p = document.createElement("p");
         p.innerHTML = "生年月日：" + data.actData[i].birthday + "<br>血液型：" + data.actData[i].bloodType + "<br>代表作：" + data.actData[i].work + "<br>WikiPedia：" + data.actData[i].wiki;
