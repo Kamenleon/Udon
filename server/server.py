d3de30c629dc695c8986a140e693ac0aaea9d410
Ryunosuke-Ikeda <ryunosuke0305@gmail.com>
2020-08-16 12:02:59 +0900

    mensindan

--- server/server.py
+++ server/server.py
@@ -7,7 +7,7 @@ import string
 import random
 
 from predict import predict
-
+from predict_men import predict2
 import sys
 import json
 
@@ -55,12 +55,36 @@ def upload():
         return render_template('ans.html',name=name,p=pasent)
 
 
+@app.route('/upload2', methods=['POST'])
+def upload2():
+    if request.files['image']:
+        # 画像として読み込み
+        stream = request.files['image'].stream
+        img_array = np.asarray(bytearray(stream.read()), dtype=np.uint8)
+        img = cv2.imdecode(img_array, 1)
 
+
+
+        # 保存
+        #dt_now = datetime.now().strftime("%Y_%m_%d%_H_%M_%S_") + random_str(5)
+        dt_now = datetime.now().strftime("test") + random_str(5)
+        save_path = os.path.join(SAVE_DIR, dt_now + ".png")
+        cv2.imwrite(save_path, img)
+        print("save", save_path)
+        print("ok---------------")
+
+        name,pasent=predict2(save_path)
+
+        #return redirect('/')
+        return render_template('ans2.html',name=name,p=pasent)
 
 @app.route('/ans', methods=['POST'])
 def post():
     return render_template('index.html')
 
+@app.route('/ans2', methods=['POST'])
+def post2():
+    return render_template('index2.html')
 
 
 if __name__ == '__main__':
